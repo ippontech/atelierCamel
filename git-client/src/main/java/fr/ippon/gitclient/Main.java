5f0085932bbc6da895330d13a41b22228bad8d33
jcdelmas <jc.delmas@gmail.com>
2012-07-14 15:36:33 +0200

    Refactoring

--- git-client/src/main/java/fr/ippon/gitclient/Main.java
+++ git-client/src/main/java/fr/ippon/gitclient/Main.java
@@ -3,30 +3,31 @@ package fr.ippon.gitclient;
 import java.io.File;
 import java.io.IOException;
 
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
+	private static final String SAMPLE_REPO_PATH = "../sampleGitRepo/.git";
+
 	public static void main(String[] args) throws Exception {
 		Repository repository = getSampleRepo();
 		new Poller(repository, new Notifier()).run();
 	}
 
-	public static Repository getSampleRepo() {
-		String gitDirPath = "../sampleGitRepo/.git";
-//		String gitDirPath = "/home/jcdelmas/dev/test-git-repo/.git";
+	public static Repository getSampleRepo() throws IOException {
 		try {
-			return getRepository(gitDirPath);
-		} catch(IOException e) {
-			e.printStackTrace();	
-			throw new RuntimeException("Le repo d'exemple n'existe pas encore. Vous pouvez le créer via le script shell initSampleGitRepo.sh à la racine du projet");
+			return getRepository(SAMPLE_REPO_PATH);
+		} catch(RepositoryNotFoundException e) {
+			throw new RuntimeException(
+					"Le repo d'exemple n'existe pas encore. " +
+					"Vous pouvez le créer via le script shell initSampleGitRepo.sh " +
+					"à la racine du projet", e);
 		}
 	}
+	
 	public static FileRepository getRepository(String gitDirPath)
 			throws IOException {
 		return new FileRepositoryBuilder()
