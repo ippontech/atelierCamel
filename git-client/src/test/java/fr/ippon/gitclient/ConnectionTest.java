3299a41bb1a3143dd17ea82e5b31ec9f9f0b623a
jcdelmas <jc.delmas@gmail.com>
2012-07-14 17:07:32 +0200

    Refactor ConnectionTest class

--- git-client/src/test/java/fr/ippon/gitclient/ConnectionTest.java
+++ git-client/src/test/java/fr/ippon/gitclient/ConnectionTest.java
@@ -2,14 +2,8 @@ package fr.ippon.gitclient;
 
 import java.io.IOException;
 
-import javax.jms.Connection;
 import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageProducer;
-import javax.jms.Queue;
-import javax.jms.Session;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.JGitInternalException;
 import org.eclipse.jgit.api.errors.NoHeadException;
@@ -20,27 +14,7 @@ public class ConnectionTest {
 
 	@Test
 	public void testConnection() throws JMSException, NoHeadException, JGitInternalException, IOException {
-		ActiveMQConnectionFactory connectionFactory = 
-				new ActiveMQConnectionFactory("tcp://localhost:61616");
-        Connection connection = connectionFactory.createConnection();
-        try {
-			connection.start();
-			
-			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			Queue queue = session.createQueue("commit");
-			
-			MessageProducer producer = session.createProducer(queue);
-			
 			Iterable<RevCommit> commits = new Git(Main.getSampleRepo()).log().call();
-			
-			Message message = session.createObjectMessage(commits.iterator().next());
-			producer.send(message);
-		} finally {
-			try {
-				connection.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+			new Notifier().notify(commits.iterator().next());
 	}
 }
